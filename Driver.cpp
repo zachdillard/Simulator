f0d6e2b35531aeedbef424f53fc9a76239004def
Zach Dillard <zachdillard@users.noreply.github.com>
2017-02-09 16:54:31 -0500

    New files
    Created Structs.h to hold data and deleted the memory class

--- Driver.cpp
+++ Driver.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
+#include "Loader.h"
+#include "Structs.h"
+#include <queue>
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string text_file;
+    Memory memory;
+    std::queue new_queue;
+    Loader loader(text_file, memory, new_queue);
     return 0;
 }
--- Loader.h
+++ Loader.h
@@ -1,13 +1,15 @@
-//
-// Created by Zach Dillard on 2/4/17.
-//
-
 #ifndef SIMULATOR_LOADER_H
 #define SIMULATOR_LOADER_H
 
+#include <string>
+#include <fstream>
+#include "Structs.h"
+#include <queue>
 
-class Loader {
-
+class Loader
+{
+public:
+    Loader(std::string file, Memory memory, std::queue new_queue);
 };
 
 
