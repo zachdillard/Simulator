745d6411e820c7906a994b29b72a5d9910ded207
Zach Dillard <zachdillard@users.noreply.github.com>
2017-02-16 13:03:55 -0500

    Update CPU.h

--- CPU.h
+++ CPU.h
@@ -1,13 +1,14 @@
-//
-// Created by Zach Dillard on 2/4/17.
-//
-
+#include <string>
 #ifndef SIMULATOR_CPU_H
 #define SIMULATOR_CPU_H
 
 
 class CPU {
-
+public:
+  //One of these registers could be the Program Counter
+  //OR maybe create a variable called PC
+  //The project specifications states that the first to registers are special purpose
+  std::string registers[16];
 };
 
 
