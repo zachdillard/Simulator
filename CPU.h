bb4c9ab1a88d75f31531f13341cba6590cce6a49
Zach Dillard <zachdillard@users.noreply.github.com>
2017-02-19 14:35:33 -0500

    Update CPU.h

--- CPU.h
+++ CPU.h
@@ -1,22 +1,78 @@
 #include <string>
+#include "Memory.h"
+#include <bitset>
+#include <sstream>
 #ifndef SIMULATOR_CPU_H
 #define SIMULATOR_CPU_H
+
+using namespace std;
 
 
 class CPU {
 //while there are instructions to fetch
-  //FETCH
+//FETCH
     //Go to the PC and get its value, this is the address of the instruction
     //Go to that address and retrieve the value
     //Give this value to DECODE
     //Update PC by 1
-  //DECODE
+	void fetch(Memory &memory, PCB &curPCB){
+	//No while test yet
+	//Gets value of current PC
+		int curPC=curPCB.pc;
+	//Gets memory value of current PC in RAM
+		string memValue = memory.getRAM(curPC);
+	//Updates PC by 1
+		curPCB.pc += 1;
+		
+	//Sends instruction to decoder
+		decoder(memValue);
+	}	
+//DECODE
     //Read each bit to figure out if I/O instruction or Compute
     //Figure out the opcode
     //Give EXECUTE something to let it know which opcode/method to execute
-  //EXECUTE
+	void decoder(string memValue){
+	//Converts hex input to binary
+		stringstream ss;
+		ss << hex << memValue;
+		unsigned placeHolder;
+		ss >> placeHolder;
+		bitset<32> ourValue(placeHolder);
+		string decodedValue = ourValue.to_string();
+	
+	//Gets our OPCODE and instruction format
+		string instructionType = getInstructionType(decodedValue);
+		
+	//Gives execute our OPCODE, the type of instruction, and the entire instruction for later use
+		execute(instructionType, decodedValue);
+	}
+
+	//PRECONDITION: The input must be a binary number in a string value
+	//POSTCONDITION: Outputs a 6-bit opcode
+	string getOpCode(string decodedValue){
+		return decodedValue.substr(2,7);
+	}
+		
+	//PRECONDITION: The input must be a binary number in a string value
+	//POSTCONDITION: Outputs 2 bits that represents the instruction type to use
+	string getInstructionType(string decodedValue){
+		return decodedValue.substr(0,2);
+	}
+		
+//EXECUTE
     //Depending on the opcode, execute that particular method or statement
-  
+	//all methods called are to-be-implemented
+	void execute(string instructionType, string statement){
+		if(instructionType == "00")
+			arithmeticFormat(statement);
+		else if(instructionType == "01")
+			conditionalFormat(statement);
+		else if(instructionType == "10")
+			jumpFormat(statement);
+		else if(instructionType == "11")
+			IOFormat(statement);
+	}
+
 public:
   //One of these registers could be the Program Counter
   //OR maybe create a variable called PC
