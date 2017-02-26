39f4bf7a1ece5444abf1463eaa55baf4a350a65c
Zach Dillard <zachdillard@users.noreply.github.com>
2017-02-25 21:09:29 -0500

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -4,17 +4,28 @@
 #include "ShortTerm.h"
 #include "Memory.h"
 #include "Loader.h"
+#include "CPU.h"
 
 int main() {
     Memory memory;
     Loader loader;
-    CPU cpu;
+    CPU cpu(&memory);
     ShortTerm shortTerm;
     //Change this to the absolute location of where you're storing the program file
     loader.readFromFile("/Users/zachdillard/School/OperatingSystems/Simulator/Simulator/programfile.txt", &memory);
     LongTerm longTerm(&memory, &shortTerm);
-    while(longTerm.getProcessLength() < memory.ramSpaceLeft())
-        longTerm.addToRam();
-    shortTerm.dispatch(&memory, &cpu);
+    while(longTerm.getProcessCount() <= memory.JobCount)
+    {
+        longTerm.setNextRamStart(0);
+        while(longTerm.getProcessCount() <= memory.JobCount && longTerm.getProcessLength() < memory.ramSpaceLeft())
+            longTerm.addToRam();
+        while(shortTerm.ready_queue.size() > 0)
+        {
+            shortTerm.dispatch(&memory, &cpu);
+            do cpu.decoder(cpu.fetch());
+            while(cpu.PC != 0);
+        }
+        memory.clearRam();
+    }
     return 0;
 }
