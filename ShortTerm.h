1293f7c5c2a1f3e88c101fbe30a293b36548cf3a
qomsday <andrew1900owen@gmail.com>
2017-02-27 15:35:34 -0500

    Update ShortTerm.h

--- ShortTerm.h
+++ ShortTerm.h
@@ -2,6 +2,7 @@
 #define PROJECT_SHORTTERM_H
 
 #include <queue>
+#include "Clock.h"
 #include "CPU.h"
 #include "Memory.h"
 
@@ -12,11 +13,17 @@ class ShortTerm {
     //Put that value into the PC of the CPU
 public:
     ShortTerm() {};
+	float endWaitTime;      //New
+	Clock waitClock;        //New
     std::queue<int> ready_queue;
     void dispatch(Memory* memory, CPU* cpu)
     {
         cpu->PC = memory->pcbs[ready_queue.front()]->pc;
+		waitClock.c = memory->pcbs[ready_queue.front()]->waitTime;  //New
+		endWaitTime = clock()-waitClock.c;                          //New
+		memory->pcbs[ready_queue.front()]->waitTime;                //New
         ready_queue.pop();
+		
     };
 };
 
